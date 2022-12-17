abaa6eec00ff4b2b9938fd155f9e5beb9e62342a
BrunoUA96 <ostapklysa@gmail.com>
2022-12-17 15:39:08 +0000

    Register, login & check user functions

---
+++ controllers/UserController.js
@@ -0,0 +1,125 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import { validationResult } from 'express-validator';
+
+import UserModule from '../models/User.js';
+
+export const register = async (req, res) => {
+   try {
+      // Провіряє полученні данні регістрації на помилки
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+         return res.status(400).json(errors.array());
+      }
+
+      // Destructuring req
+      const { fullName, email, password, avatarUrl } = req.body;
+
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(password, salt);
+
+      // Define User document
+      const doc = new UserModule({
+         fullName,
+         email,
+         passwordHash: hash,
+         avatarUrl,
+      });
+
+      // Save User
+      const user = await doc.save();
+
+      const token = jwt.sign(
+         {
+            _id: user._id,
+         },
+         'secret123',
+         {
+            expiresIn: '30d',
+         },
+      );
+
+      // Remove password from response
+      const { passwordHash, ...userData } = user._doc;
+
+      res.json({
+         ...userData,
+         token,
+      });
+   } catch (err) {
+      console.log(err);
+      res.status(500).json({
+         message: 'Failed to register',
+      });
+   }
+};
+
+export const login = async (req, res) => {
+   try {
+      // Find User in MongoDB
+      const user = await UserModule.findOne({ email: req.body.email });
+
+      // If user not found
+      if (!user) {
+         return res.status(404).json({
+            message: 'User not faund',
+         });
+      }
+
+      // Check password (compare req pass & MongoDB pass), use bcrypt
+      const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+
+      if (!isValidPass) {
+         return res.status(400).json({
+            message: 'Wrong login or password',
+         });
+      }
+
+      const token = jwt.sign(
+         {
+            _id: user._id,
+         },
+         'secret123',
+         {
+            expiresIn: '30d',
+         },
+      );
+
+      // Remove pass from user doc
+      const { passwordHash, ...userData } = user._doc;
+
+      res.json({
+         ...userData,
+         token,
+      });
+   } catch (error) {
+      console.log(error);
+      res.status(500).json({
+         message: 'Failed to register',
+      });
+   }
+};
+
+export const authMe = async (req, res) => {
+   try {
+      // Find User in MongoDB
+      const user = await UserModule.findById(req.userId);
+
+      if (!user) {
+         return res.status(404).json({
+            message: 'User not found',
+         });
+      }
+
+      const { passwordHash, ...userData } = user._doc;
+
+      res.json(userData);
+   } catch (error) {
+      console.log(error);
+      res.status(500).json({
+         message: 'Failed to register',
+      });
+   }
+};
--- index.js
+++ index.js
@@ -1,36 +1,29 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
+
 import mongoose from 'mongoose';
 
+import { registerValidation } from './validations/auth.js';
+
+import checkAuth from './utils/checkAuth.js';
+
+import * as UserController from './controllers/UserController.js';
+
 mongoose
-   .connect('mongodb+srv://admin:123@cluster0.aldlull.mongodb.net/?retryWrites=true&w=majority')
+   .set('strictQuery', true)
+   .connect('mongodb+srv://admin:123@cluster0.aldlull.mongodb.net/blog?retryWrites=true&w=majority')
    .then(() => console.log('DB OK'))
    .catch((err) => console.log('DB ERROR: ', err));
 
 const app = express();
 app.use(express.json());
-
-app.get('/', (req, res) => {
-   res.send('Hello World');
-});
-
 // Login
-app.post('/auth/login', (req, res) => {
-   console.log(req);
+app.post('/auth/login', UserController.login);
 
-   const token = jwt.sign(
-      {
-         email: req.body.email,
-         password: req.body.password,
-      },
-      'secret123',
-   );
+// Check User
+app.get('/auth/me', checkAuth, UserController.authMe);
 
-   res.json({
-      success: true,
-      token,
-   });
-});
+// Register
+app.post('/auth/register', registerValidation, UserController.register);
 
 app.listen(4444, (err) => {
    if (err) {
